710f94a0ee3e5fc3f50c3d8e45c37f63cd1aa948
Odyssefs Aristeidou <airoddyisback@gmail.com>
2016-10-01 19:21:40 +0200

    Init Commit

---
+++ src/main/java/de/oaristeidou/cleancode/Main.java
@@ -0,0 +1,11 @@
+package de.oaristeidou.cleancode;
+
+/**
+ * Created by odyssefs on 01.10.16.
+ */
+public class Main {
+    public static void main (String []args){
+        System.out.println("Hello World");
+    }
+
+}
