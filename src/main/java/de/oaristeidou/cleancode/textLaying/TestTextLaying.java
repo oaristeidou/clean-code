8493e90e6110c398443dd2d7c3b2b30241031fa3
Odyssefs Aristeidou <airoddyisback@gmail.com>
2016-11-19 19:22:32 +0100

    - Make the design - Develop the test methods and the implimentation of the first design step

--- src/main/java/de/oaristeidou/cleancode/textLaying/TestTextLaying.java
+++ src/main/java/de/oaristeidou/cleancode/textLaying/TestTextLaying.java
@@ -3,6 +3,9 @@ package de.oaristeidou.cleancode.textLaying;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 /**
@@ -14,18 +17,14 @@ public class TestTextLaying {
             "Es blaut die Nacht,\n" +
                     "die Sternlein  blinken,\n" +
                     "Schneeflöcklein  leis hernieder sinken.";
-
     private static final String CLEANTEXT =
             "Es blaut die Nacht, " +
                     "die Sternlein blinken, " +
                     "Schneeflöcklein leis hernieder sinken.";
-
     private static final String CLEANED_TEXT = "Es blaut die Nacht, die Sternlein blinken, Schneeflöcklein leis hernieder sinken.";
     private static final String[] CLEANED_TEXT_ARRAY = {
             "Es", "blaut", "die", "Nacht,", "die", "Sternlein", "blinken,", "Schneeflöcklein", "leis", "hernieder", "sinken."
     };
-
-
     private static final String TEST_TEXT_RESULT =
             "Es blaut die\n" +
                     "Nacht, die\n" +
@@ -49,6 +48,43 @@ public class TestTextLaying {
     @Test(dataProvider = "dataSplitTextToWords")
     public void testSplitTextToWords(String cleanText, String expected[]) {
         assertEquals(TextLaying.splitTextToWords(cleanText), expected);
+    }
+
+    @Test(dataProvider = "dataApplyLineLayout")
+    public void testApplyLineLayout(String words[], int maxParagraphLegth, List<String> expected) {
+        assertEquals(TextLaying.applyLineLayout(words, maxParagraphLegth), expected);
+    }
+
+    @Test(dataProvider = "dataAppendLinesToText")
+    public void testAppendLinesToText(List<String> lines, String expected) {
+        assertEquals(TextLaying.appendLinesToText(lines), expected);
+    }
+
+    @DataProvider
+    public Object[][] dataApplyLineLayout() {
+        return new Object[][]{
+                {CLEANED_TEXT_ARRAY, Integer.valueOf(13), getListOfLinesText()}
+        };
+    }
+
+    @DataProvider
+    public Object[][] dataAppendLinesToText() {
+        return new Object[][]{
+                {getListOfLinesText(), TEST_TEXT_RESULT}
+        };
+    }
+
+    private List getListOfLinesText() {
+        List linesOfText = new ArrayList<String>();
+        linesOfText.add("Es blaut die\n");
+        linesOfText.add("Nacht, die\n");
+        linesOfText.add("Sternlein\n");
+        linesOfText.add("blinken,\n");
+        linesOfText.add("Schneeflöcklei\n");
+        linesOfText.add("n leis\n");
+        linesOfText.add("hernieder\n");
+        linesOfText.add("sinken.");
+        return linesOfText;
     }
 
     @DataProvider
--- src/main/java/de/oaristeidou/cleancode/textLaying/TextLaying.java
+++ src/main/java/de/oaristeidou/cleancode/textLaying/TextLaying.java
@@ -1,32 +1,65 @@
 package de.oaristeidou.cleancode.textLaying;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * Created by odyssefs on 06.11.16.
  */
 public class TextLaying {
 
-    public String textLaying (String text, int maxParagraphLegth){
+    public String textLaying(String text, int maxParagraphLegth) {
         String cleanText = cleanText(text);
         String[] words = splitTextToWords(cleanText);
         return applytextLayout(words, maxParagraphLegth);
     }
 
-    public static String cleanText (String text){
-        return text.replaceAll("\\s+"," ").replace(System.lineSeparator(), "");
+    public static String cleanText(String text) {
+        return text.replaceAll("\\s+", " ").replace(System.lineSeparator(), "");
     }
 
-    public static String [] splitTextToWords (String cleanText){
+    public static String[] splitTextToWords(String cleanText) {
         return cleanText.split(" ");
     }
 
-    public String applytextLayout (String [] words, int maxParagraphLegth){
+    public static String applytextLayout(String[] words, int maxParagraphLegth) {
         return "";
     }
 
-    public List<String> applyLineLayout (String word[]){
+    public static List<String> applyLineLayout(String words[], int maxParagraphLegth) {
+        List textLines = new ArrayList<String>();
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            int newLineIndex = 0;
+            if (words[i].length() + 1 < maxParagraphLegth && builder.length() < maxParagraphLegth){
+                builder.append(words[i]);
+                builder.append(" ");
+            }
+            else{
+                builder.append(words[i].substring(maxParagraphLegth));
+            }
+
+            if (builder.length() >= maxParagraphLegth){
+                textLines.add(builder.toString().trim() + "\n");
+                builder = new StringBuilder();
+            }
+
+        }
+
+        return textLines;
+    }
+
+    public static List<String> applyJustifyLayout(List<String> lines) {
         return null;
+    }
+
+    public static String appendLinesToText(List<String> lines) {
+        StringBuilder builder = new StringBuilder();
+        for (String line : lines)
+            builder.append(line);
+        return builder.toString();
     }
 
 
